7b3fccfbb58916c15ed6e6019052ebba2548dcf0
sirjoeyyy <123411320+sirjoeyyy@users.noreply.github.com>
2023-10-10 21:54:25 +0200

    Update nginxpart1.sh

--- nginxpart1.sh
+++ nginxpart1.sh
@@ -1,1 +1,31 @@
-s7y8uio
+//HOW TO INTALL NGINX
+sudo apt-get update 
+
+sudo apt-get -y intall nginx
+
+systemctl is-active nginx
+
+sudo systemctl start nginx
+sudo systemctl stop nginx
+
+
+//HOW TO CREATE DIRECTORY 
+ls
+cd
+sudo mkdir /var/www/html/labs 
+ls-ld
+
+//HOW TO CHANGE OWNERSHIP
+sudo chown (new ownwer):root /var/www/html/labs
+ls-ld /var/www/html/labs
+
+//HOW TO CREATE A SYMBOLIC LINK
+ln-s
+ls-l
+cd html 
+
+//HOW TO CREATE AN INDEX.HTML FILE
+nano index 
+
+
+
