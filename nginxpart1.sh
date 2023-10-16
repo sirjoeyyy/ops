ed7aeeadb8f5518612cf583ea5293171d3df1c86
Sinenhlanhla-1604 <146891187+Sinenhlanhla-1604@users.noreply.github.com>
2023-10-17 00:06:46 +0200

    Update nginxpart1.sh

--- nginxpart1.sh
+++ nginxpart1.sh
@@ -3,7 +3,7 @@
 #! KM Sejoe & AC Ntini
 
 
-sudo apt-get update 
+sudo apt-get update # Update package information to ensure the latest packages are available.
 
 sudo apt-get -y install nginx
 
@@ -17,24 +17,24 @@ sudo systemctl stop nginx
 ls
 cd
 sudo mkdir /var/www/html/labs 
-ls-ld /var/www/html/labs
+ls -ld /var/www/html/labs
 
 
 sudo chown (new ownwer):root /var/www/html/labs
-ls -ld /var/www/html/labs 
+ls -ld /var/www/html/labs (
 
 
 ln -s /var/www/html/labs html
 ls -l
 cd html 
-touch index.html
-nano index.html
+sudo touch index.html
+sudo nano index.html
 
 
 cat index.html
-mkdir bin 
+sudo mkdir bin 
 cd bin
-touch websrv_coonfig_script.bash
+touch websrv_config_script.bash
 ls -l
 chmod u+x websrv_config_script.bash
 ls -l
