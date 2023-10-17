54843bcd9f07fa308fb815ad276f556410a92d27
Sinenhlanhla-1604 <146891187+Sinenhlanhla-1604@users.noreply.github.com>
2023-10-17 02:28:55 +0200

    Update modified_bash_script.sh

--- modified_bash_script.sh
+++ modified_bash_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#Variable Declaration
 dir="labs" #variable "labs" created
 link="html" #symbolic link variable
 page="index.html" #html variable
@@ -7,22 +7,21 @@ path="/var/www/html"
 user="student"
 home="/home/student"
 echo"NGINX CONFIG SCRIPT"
-echo"##################"
+echo"-------------------"
 echo"Script Starting"
 sleep 1 #delays jump to next line
 
 rn -rvf $path/$dir
 rn -rvf $home/$link
 
-if [ "$(systemctl is-active nginx 2> /dev/null)" = "active" ]
+if [ "'systemctl is-active nginx 2> /dev/null'" = "active" ]
 then
         echo "NGINX service running, continue with configuration..."
         sleep 1
         echo "Creating directory $dir"
         sleep 1
-        rn -rf $path/$dir
+       
         mkdir $path/$dir
-
         if [-d $path/$dir]
         then 
                 echo"Changing Ownership of $dir to $USER"
@@ -43,11 +42,9 @@ then
         if[-L $home/$link ]; then
         cd $home/$link
         echo "Creating test page $page"
-        touch $page
-        sleep 1
+        touch $page #displays html
         echo "Adding dummy content to $page"
         sleep 1
-        cd ..$link
         echo "<html>" >> $page
         echo "<body>" >> $page
         echo "<h1>STUDENT INFORMATION</h1>" >> $page
@@ -56,10 +53,18 @@ then
         echo "</html>" >> $page
         
         echo"Script has finished executing page"
-        cat ../html/$page
-        
-else 
-        echo "NGINX service not running"
-        echo "Exiting Script"
-        exit 1 #exits script
+        else
+                echo"Symbolic link ($link) was not found!"
+                echo"Script will now exit..."
+                sleep 1
+                exit 1
+
 fi
+        
+        else 
+                echo "NGINX service not running"
+                echo "Exiting Script"
+                exit 1 #exits script
+fi
+
+#End of Script
