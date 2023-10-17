ee37cad76f65dd8112ed4b83cd96945ffc90cebd
Sinenhlanhla-1604 <146891187+Sinenhlanhla-1604@users.noreply.github.com>
2023-10-17 02:12:49 +0200

    Update modified_bash_script.sh

--- modified_bash_script.sh
+++ modified_bash_script.sh
@@ -3,40 +3,60 @@
 dir="labs" #variable "labs" created
 link="html" #symbolic link variable
 page="index.html" #html variable
+path="/var/www/html"
+user="student"
+home="/home/student"
 echo"NGINX CONFIG SCRIPT"
 echo"##################"
 echo"Script Starting"
 sleep 1 #delays jump to next line
 
-if (systemctl is-active nginx)
+rn -rvf $path/$dir
+rn -rvf $home/$link
+
+if [ "$(systemctl is-active nginx 2> /dev/null)" = "active" ]
 then
         echo "NGINX service running, continue with configuration..."
         sleep 1
         echo "Creating directory $dir"
         sleep 1
-        mkdir /var/www/html/$dir
+        rn -rf $path/$dir
+        mkdir $path/$dir
+
+        if [-d $path/$dir]
+        then 
+                echo"Changing Ownership of $dir to $USER"
+        else 
+                echo"Did not find directory $path/$dir"
+                echo"Exiting Script..."
+                exit 1
+        fi
+        sleep 1
         echo "Changing ownership of $dir to $USER"
         sleep 1
-        chown $USER:root /var/www/html/labs
-        echo "Creating symbolic link"
+        chown $USER:root $path/$dir
+        echo "Creating symbolic link $home/$link"
         sleep 1
-        ln -s /var/www/html/$dir $link
+        ln -s $path/$dir $home/$link
+        sleep 1
+
+        if[-L $home/$link ]; then
+        cd $home/$link
         echo "Creating test page $page"
+        touch $page
         sleep 1
         echo "Adding dummy content to $page"
         sleep 1
-        echo "<html><body><br><h1>STUDENT INFORMATION</h1><br><p>Names:<br>Student no:<br>Course:<br></p></body></html>" >> $page
+        cd ..$link
+        echo "<html>" >> $page
+        echo "<body>" >> $page
+        echo "<h1>STUDENT INFORMATION</h1>" >> $page
+        echo "<p>Names: Student no: Course:</p>" >> $page
+        echo "</body>" >> $page
+        echo "</html>" >> $page
         
-                
-                  
-                  
-                  
-                  
-                  
-                  
-               
-           
-        
+        echo"Script has finished executing page"
+        cat ../html/$page
         
 else 
         echo "NGINX service not running"
