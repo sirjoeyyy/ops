3566f5fdb3b47cc2cabe3291e34715c514d464e7
Sinenhlanhla-1604 <146891187+Sinenhlanhla-1604@users.noreply.github.com>
2023-10-19 23:43:10 +0200

    Update nginxpart1.sh

--- nginxpart1.sh
+++ nginxpart1.sh
@@ -1,32 +1,52 @@
 #!/bin/bash
 #! KM Sejoe & AC Ntini
 
-
+# Update package information to ensure the latest packages are available.
 sudo apt-get update # Update package information to ensure the latest packages are available.
 
+# Install Nginx
 sudo apt-get -y install nginx
 
+# Check if Nginx is active
 systemctl is-active nginx
 
+# Start Nginx
 sudo systemctl start nginx
+
+# Stop Nginx
 sudo systemctl stop nginx
 
+# Create a directory at /var/www/html/labs
 sudo mkdir /var/www/html/labs 
+
+# List the contents of the /var/www/html directory
 ls -ld /var/www/html/labs
 
+# Change ownership of the /var/www/html/labs directory to a new owner (replace <new_owner> with the actual owner)
+sudo chown <new_owner>:root /var/www/html/labs
 
-sudo chown (new ownwer):root /var/www/html/labs
-ls -ld /var/www/html/labs (
+# List the contents and permissions of the /var/www/html/labs directory
+ls -ld /var/www/html/labs 
 
-
+# Create a symbolic link to the /var/www/html/labs directory
 ln -s /var/www/html/labs html
-ls -l
+
+# List the symbolic link and its target
+ls -l /var/www/html/labs html
+
+# Navigate to the /var/www/html directory
 cd /var/www/html 
-sudo touch index.html #creating html file
+
+#creating html file
+sudo touch index.html 
+
+#Edit html file
 sudo nano index.html
 
+#displays html code
+cat index.html 
 
-cat index.html #displays html
+
 sudo mkdir bin 
 cd bin
 sudo touch websrv_config_script.bash #create bash script file
